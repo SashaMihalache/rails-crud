a9fa53d3718c03d360fd5c30c06a0b71dcc291fb
Sasha Mihalache <sasha.mihalache@gmail.com>
2016-12-29 13:19:44 +0200

    Add 1-n assocation between user and articles, rubocop linter and debugger from vsc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,51 +1,53 @@
 class ArticlesController < ApplicationController
-    before_action :set_article, only: [:edit, :update, :show, :destroy]
-    
-    def index
-        @articles = Article.all
-    end
-    
-    def new
-        @article = Article.new     
-    end 
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-    def edit
-    end
+  def index
+    @articles = Article.all
+  end
 
-    def create
-        # render plain: params[:article].inspect
-        @article = Article.new(article_params)
-        if @article.save
-            flash[:success] = "Article was successfully created"
-            redirect_to article_path(@article) #from rails routes
-        else
-            render 'new'
-        end
-    end
+  def new
+    @article = Article.new
+  end
 
-    def update
-        if @article.update(article_params)
-            flash[:info] = "Article was successfully updated"
-            redirect_to article_path(@article)
-        else
-            render 'edit'
-        end
-    end
+  def edit; end
 
-    def show
+  def create
+    # render plain: params[:article].inspect
+    # debugger
+    @article = Article.new(article_params)
+    @article.user = User.first
+    if @article.save
+      flash[:success] = 'Article was successfully created'
+      redirect_to article_path(@article) # from rails routes
+    else
+      render 'new'
     end
+  end
 
-    def destroy
-        @article.destroy
-        flash[:danger] = "Article was successfully deleted"
-        redirect_to articles_path
+  def update
+    if @article.update(article_params)
+      flash[:info] = 'Article was successfully updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
+  end
 
-    private
-        def set_article
-            @article = Article.find(params[:id])
-        end
-        def article_params
-            params.require(:article).permit(:title, :description)
-        end
+  def show; end
+
+  def destroy
+    @article.destroy
+    flash[:danger] = 'Article was successfully deleted'
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
+# Article model
 class Article < ActiveRecord::Base
-    validates :title, presence: true, length: { minumum: 3, maximum: 50 } #must have title
-    validates :description, presence: true, length: { minumum: 10, maximum: 300 }
+  belongs_to :user
+  validates :title,
+            presence: true,
+            length: { minumum: 3, maximum: 50 } # must have title
+  validates :description, presence: true, length: { minumum: 10, maximum: 300 }
+  validates :user_id, presence: true
 end
